22dc24fa4924fffccc799539a31902cdac3cfff0
jvcoutinho <jv.coutinho@victorconsultoria.com.br>
2021-02-18 10:36:34 -0300

    Initial Commit

---
+++ Backend/BlueModas.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BlueModas.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ Backend/BlueModas.Repository/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BlueModas.Repository
+{
+    public class Class1
+    {
+    }
+}
